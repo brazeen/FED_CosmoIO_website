ebef5aadb5448da18b474f4878944180b466c5e4
Yang Yi Toh <s10258639@connect.np.edu.sg>
2024-01-19 20:41:53 +0800

    transferred api from script.js to apiscript

--- JS/script.js
+++ JS/script.js
@@ -133,35 +133,3 @@ window.onmousemove = function(ev){
 
 //STORY PAGE FOR TEXTBOX--------------------------------------------------------------------------------------------------------------------
 
-//API--------------------------------------------------------------------------------------------------------------------
-var category = 'music';
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/trivia?category=' + category,
-    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
-
-
-
-[
-    {
-      "category": "mathematics",
-      "question": "What is the minimum number of integer degrees in a reflex angle?",
-      "answer": "181"
-    }
-  ]
-  
-  [
-    {
-      "category": "mathematics",
-      "question": "What geometric shape has 4 equal sides",
-      "answer": "Square"
-    }
-  ]
