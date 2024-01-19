f7c8b69972139b6dfe1e5c8b30cc7a632a0bd984
Yang Yi Toh <s10258639@connect.np.edu.sg>
2024-01-19 23:02:29 +0800

    added more data to apiscript

--- JS/apiscript.js
+++ JS/apiscript.js
@@ -1,5 +1,5 @@
 //API--------------------------------------------------------------------------------------------------------------------
-var category = 'music';
+var category = 'mathematics';
 $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/trivia?category=' + category,
@@ -15,7 +15,7 @@ $.ajax({
 
 
 
-[
+  [
     {
       "category": "mathematics",
       "question": "What is the minimum number of integer degrees in a reflex angle?",
@@ -30,3 +30,61 @@ $.ajax({
       "answer": "Square"
     }
   ]
+  [
+    {
+      "category": "mathematics",
+      "question": "The mathematical study of properties of lines, angels, etc., is ________.",
+      "answer": "Geometry"
+    }
+  ]
+  [
+    {
+      "category": "mathematics",
+      "question": "The angles inside a square total _______ degrees.",
+      "answer": "360"
+    }
+  ]
+  [
+    {
+      "category": "mathematics",
+      "question": "What is the minimum number of integer degrees in an acute angle?",
+      "answer": "One"
+    }
+  ]
+  [
+    {
+      "category": "mathematics",
+      "question": "Benoit Mandelbrot discovered what mathematical structures?",
+      "answer": "Fractals"
+    }
+  ]
+  [
+    {
+      "category": "mathematics",
+      "question": "What is next in the series 1 8 27 ?? 125 216?",
+      "answer": "64"
+    }
+  ]
+  [
+    {
+      "category": "mathematics",
+      "question": "What is the square root of 1?",
+      "answer": "1"
+    }
+  ] 
+  [
+    {
+      "category": "mathematics",
+      "question": "What is 65% of 60?",
+      "answer": "39"
+    }
+  ]
+  [
+    {
+      "category": "mathematics",
+      "question": "How many corners are there in a cube?",
+      "answer": "Eight"
+    }
+  ]
+  
+ 
