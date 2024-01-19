fd99eae1ebbf471d9ec58252b86d71793a1f6b26
Yang Yi Toh <s10258639@connect.np.edu.sg>
2024-01-20 00:15:58 +0800

    successfully fetched and printed the category, question and answer into the console from api ninja using API AJAX

--- JS/apiscript.js
+++ JS/apiscript.js
@@ -1,90 +1,24 @@
 //API--------------------------------------------------------------------------------------------------------------------
 var category = 'mathematics';
+//retrieve data by AJAX from api-ninja
 $.ajax({
     method: 'GET',
     url: 'https://api.api-ninjas.com/v1/trivia?category=' + category,
-    headers: { 'X-Api-Key': 'YOUR_API_KEY'},
+    headers: { 'X-Api-Key': 'nz8vVKrPrL7wvqs9iPp1EQ==tvjazCHgm8egFldd'},
     contentType: 'application/json',
     success: function(result) {
         console.log(result);
+      
+        
     },
     error: function ajaxError(jqXHR) {
         console.error('Error: ', jqXHR.responseText);
     }
 });
+//print out category, question and answer by using a for loop
+for (var i = 0; i < questions.length; i++) {
+  console.log("Category: " + questions[i].category);
+  console.log("Question: " + questions[i].question);
+  console.log("Answer: " + questions[i].answer);
+}
 
-
-
-  [
-    {
-      "category": "mathematics",
-      "question": "What is the minimum number of integer degrees in a reflex angle?",
-      "answer": "181"
-    }
-  ]
-  
-  [
-    {
-      "category": "mathematics",
-      "question": "What geometric shape has 4 equal sides",
-      "answer": "Square"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "The mathematical study of properties of lines, angels, etc., is ________.",
-      "answer": "Geometry"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "The angles inside a square total _______ degrees.",
-      "answer": "360"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "What is the minimum number of integer degrees in an acute angle?",
-      "answer": "One"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "Benoit Mandelbrot discovered what mathematical structures?",
-      "answer": "Fractals"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "What is next in the series 1 8 27 ?? 125 216?",
-      "answer": "64"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "What is the square root of 1?",
-      "answer": "1"
-    }
-  ] 
-  [
-    {
-      "category": "mathematics",
-      "question": "What is 65% of 60?",
-      "answer": "39"
-    }
-  ]
-  [
-    {
-      "category": "mathematics",
-      "question": "How many corners are there in a cube?",
-      "answer": "Eight"
-    }
-  ]
-  
- 
